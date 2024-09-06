bcfe5a352094c252f53e600302915a11eecbd983
priyadharshini20030311 <sdharshinipriya77@gmail.com>
2024-09-06 15:33:51 +0530

    python workouts

---
+++ Type casting.py
@@ -0,0 +1,36 @@
+'''a=input("Enter one value:")
+print(type(a))'''
+
+'''a=int(a)
+print(type(a))'''
+
+
+
+'''(AND OR NOT)'''
+
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Enter third number"))
+if(a>b)and(a>c):
+    print("true")
+else:
+    print("false")
+
+
+'''a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Enter third number"))
+if(a>b)or(a>c):
+    print("A")
+else:
+    print("false")'''
+
+
+'''a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+if not(a>b):
+    print("Hello")
+else:
+    print("hii")'''
+
+
---
+++ priya.py
@@ -0,0 +1,16 @@
+a=(input("enter the value one"))
+b=(input("enter the value two"))
+c=(input("enter the value three"))
+if(a>b):
+    if(a>c):
+        print("A is bigger than")
+    else:
+        print("A is lesser than")
+else:
+    if(b>c):
+        print("B is bigger than")
+    else:
+        print("B is lesser than")
+        
+          
+      
